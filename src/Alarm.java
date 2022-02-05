c94e4f0865655c5e772eb99f0408ce8fbbe95864
JavaConterry <conterry05@gmail.com>
2022-02-05 16:56:29 +0200

    spinner added

--- src/Alarm.java
+++ src/Alarm.java
@@ -1,24 +1,28 @@
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class Alarm {
 
-    String[] timeStrings = { "10.00", "11.00", "12.00", "13.00", "14.00" };
-    private JComboBox preferredTime = new JComboBox(timeStrings);
     private JTextArea timeBeforeRinging;
     private JButton applyButton;
     private JPanel mainPanel;
-
+    private JSpinner timeSpinner;
 
     public Alarm() {
+        System.out.println("a3");
+        createSpinner();
         applyButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "");
+                JOptionPane.showMessageDialog(null, "fuckin shit");
             }
         });
     }
@@ -29,7 +33,7 @@ public class Alarm {
         frame.setContentPane(new Alarm().mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-
+        System.out.println("a1");
         //Display the window.
         frame.pack();
         frame.setVisible(true);
@@ -43,5 +47,16 @@ public class Alarm {
         });
     }
 
+    private void createSpinner() {
+        System.out.println("a2");
+        Date date = new Date();
+        SpinnerDateModel dateModel = new SpinnerDateModel(date, null, null, Calendar.HOUR_OF_DAY);
+        System.out.println(dateModel.getDate());
+        this.timeSpinner = new JSpinner(dateModel);
+        System.out.println(timeSpinner.getValue());
+        JSpinner.DateEditor dateEditor = new JSpinner.DateEditor(timeSpinner, "HH:mm:ss");
+        timeSpinner.setEditor(dateEditor);
+        System.out.println(timeSpinner.getValue());
 
+    }
 }
